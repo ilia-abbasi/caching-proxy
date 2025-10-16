add7de08782280b3769295182818fd989d5e3fb9
Ilia Abbasi <81512402+ilia-abbasi@users.noreply.github.com>
2025-10-16 17:52:21 +0330

    Created types.ts and added DEFAULT_PORT in index.js

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,9 @@ import { Command } from "commander";
 import packageData from "../package.json";
 import config from "./helpers/config.js";
 import { createApp } from "./app/app.js";
+import { Options } from "./helpers/types.js";
 
+const DEFAULT_PORT = 7575;
 const program = new Command();
 
 program
@@ -12,10 +14,21 @@ program
   .version(packageData.version);
 
 program
-  .option("-p, --port <port-number>", "Port number to listen on")
+  .option(
+    "-p, --port <port-number>",
+    "Port number to listen on",
+    parseInt,
+    DEFAULT_PORT
+  )
   .argument("<host>", "The host to cache responses from");
 
 program.parse();
 config.host = program.args[0]!;
 
-const options = program.opts();
+const options: Options = program.opts();
+const app = createApp();
+const port = options.port;
+
+app.listen(() => {
+  console.log(`Listening on port ${port}`);
+});
