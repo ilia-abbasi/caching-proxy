474a566b2b540b46b5c555cbabc77ca21b6ebc6e
Ilia Abbasi <81512402+ilia-abbasi@users.noreply.github.com>
2025-10-16 17:08:53 +0330

    Fixed commander code

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import { Command } from "commander";
+import packageData from "../package.json";
 const program = new Command();
-console.log(process.argv);
 
 program
-  .option("-p, --port <port_number>", "Port number to listen on")
+  .name(packageData.name)
+  .description(packageData.description)
+  .version(packageData.version);
+
+program
+  .option("-p, --port <port-number>", "Port number to listen on")
   .argument("<host>", "The host to cache responses from");
+
+program.parse();
 
 const options = program.opts();
 
