982bfc310bf3232e48dec04e1307966ac5346c02
Ilia Abbasi <81512402+ilia-abbasi@users.noreply.github.com>
2025-10-16 15:48:37 +0330

    Added config files for ts and jest
    - Installed more packages
    - Added index.js

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+import { createDefaultPreset } from "ts-jest";
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+/** @type {import("jest").Config} **/
+export default {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+  transform: {
+    ...tsJestTransformCfg,
+  },
+  testPathIgnorePatterns: ["/node_modules/", "/dist/"],
+};
